4139da1b4bbabf57fee9093189485cc64e093787
Subin-Panta <subinpanta9@gmail.com>
2020-05-03 21:45:45 +0545

    passed copy of data instead of reference thorugh context

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,10 @@
-import React, { createContext ,useState} from 'react'
+import React, { createContext ,useState,useEffect} from 'react'
 import {storeProducts,detailProduct} from './data'
 import Product from './components/Product'
 export const ProductContext= createContext()
 
 export default function ProductProvider(props) {
-   const [products,setProducts]=useState(storeProducts)
+   const [products,setProducts]=useState([])
    const[productdetail,setProductdetail]=useState(detailProduct)
    const handleDetail =()=>{
        console.log("hello form detail")
@@ -12,6 +12,17 @@ export default function ProductProvider(props) {
    const addToCart =()=>{
     console.log("hello form addtocart")
    }
+    const seproducts =() => {
+       let products =[]
+       storeProducts.forEach(item => {
+           const singleItem ={...item}
+           products = [...products,singleItem]
+       })
+       setProducts(products)
+   }
+   useEffect(()=>{
+       seproducts()
+   })
     return (
         <ProductContext.Provider value={{products,productdetail,handleDetail,addToCart}}>
           {props.children}
